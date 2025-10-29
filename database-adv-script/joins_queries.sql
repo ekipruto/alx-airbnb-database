6546413828b38883dff9e6585a883be9e5afcf81
eKipruto <edmond.kipruto@gmail.com>
2025-10-29 16:01:50 +0300

    fix: simplify LEFT JOIN query for ALX checker

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,20 +1,14 @@
--- TASK 0: COMPLEX QUERIES WITH JOINS
--- File: joins_queries.sql
--- Purpose: Demonstrate INNER JOIN, LEFT JOIN, and FULL OUTER JOIN
--- Database: alx_airbnb_db
--- NOTE: Use this version if your tables are created with quotes ("User", "Booking", etc.)
 -- QUERY 1: INNER JOIN
 -- Retrieve all bookings and the respective users who made those bookings
 
 SELECT 
     "Booking".booking_id,
     "Booking".property_id,
-    "Booking".user_id,
     "Booking".start_date,
     "Booking".end_date,
     "Booking".total_price,
     "Booking".status,
-    "User".user_id AS booking_user_id,
+    "User".user_id,
     "User".first_name,
     "User".last_name,
     "User".email,
@@ -30,17 +24,12 @@ INNER JOIN
 
 SELECT 
     "Property".property_id,
-    "Property".host_id,
     "Property".name,
-    "Property".description,
     "Property".location,
     "Property".pricepernight,
     "Review".review_id,
-    "Review".property_id AS review_property_id,
-    "Review".user_id AS reviewer_id,
     "Review".rating,
-    "Review".comment,
-    "Review".created_at AS review_date
+    "Review".comment
 FROM 
     "Property"
 LEFT JOIN 
@@ -56,11 +45,9 @@ SELECT
     "User".first_name,
     "User".last_name,
     "User".email,
-    "User".phone_number,
     "User".role,
     "Booking".booking_id,
     "Booking".property_id,
-    "Booking".user_id AS booking_user_id,
     "Booking".start_date,
     "Booking".end_date,
     "Booking".total_price,
