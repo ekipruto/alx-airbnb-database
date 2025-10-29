b414aaaa85b2ed135e28cb9d1b8a2eead47efca4
eKipruto <edmond.kipruto@gmail.com>
2025-10-29 17:20:59 +0300

    Just corrected again on left join scripts to as per error on ALX Checker

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,58 +1,47 @@
--- QUERY 1: INNER JOIN
--- Retrieve all bookings and the respective users who made those bookings
-
-SELECT 
-    "Booking".booking_id,
-    "Booking".property_id,
-    "Booking".start_date,
-    "Booking".end_date,
-    "Booking".total_price,
-    "Booking".status,
-    "User".user_id,
-    "User".first_name,
-    "User".last_name,
-    "User".email,
-    "User".phone_number,
-    "User".role
-FROM 
-    "Booking"
-INNER JOIN 
-    "User" ON "Booking".user_id = "User".user_id;
-
--- QUERY 2: LEFT JOIN
--- Retrieve all properties and their reviews, including properties that have no reviews
-
-SELECT 
-    "Property".property_id,
-    "Property".name,
-    "Property".location,
-    "Property".pricepernight,
-    "Review".review_id,
-    "Review".rating,
-    "Review".comment
-FROM 
-    "Property"
-LEFT JOIN 
-    "Review" ON "Property".property_id = "Review".property_id;
+-- Query 1: INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+SELECT
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM
+    "Booking" AS b
+INNER JOIN
+    "User" AS u ON b.user_id = u.user_id;
 
 
--- QUERY 3: FULL OUTER JOIN
--- Retrieve all users and all bookings, even if the user has no booking 
--- or a booking is not linked to a user
+    -- Query 2: LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+SELECT
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_date
+FROM
+    "Property" AS p
+LEFT JOIN
+    "Review" AS r ON p.property_id = r.property_id
+ORDER BY
+    p.property_id;
 
-SELECT 
-    "User".user_id,
-    "User".first_name,
-    "User".last_name,
-    "User".email,
-    "User".role,
-    "Booking".booking_id,
-    "Booking".property_id,
-    "Booking".start_date,
-    "Booking".end_date,
-    "Booking".total_price,
-    "Booking".status
-FROM 
-    "User"
-FULL OUTER JOIN 
-    "Booking" ON "User".user_id = "Booking".user_id;
+
+    -- Query 3: FULL OUTER JOIN to retrieve all users and all bookings, 
+-- even if the user has no booking or a booking is not linked to a user.
+SELECT
+    u.user_id,
+    u.email AS user_email,
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price
+FROM
+    "User" AS u
+FULL OUTER JOIN
+    "Booking" AS b ON u.user_id = b.user_id;
